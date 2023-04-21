4db5bd739da29ac18994d13f8581976bd2ea4aad
root <aronimoademola@gmail.com>
2023-04-21 03:11:19 -0700

    er

---
+++ 0x0A-configuration_management/README.m
@@ -0,0 +1,10 @@
+0x0A. Configuration management
+DevOps
+SysAdmin
+Scripting
+CI/CD
+ By: Sylvain Kalache
+ Weight: 1
+ Project will start Apr 21, 2023 6:00 AM, must end by Apr 22, 2023 6:00 AM
+ Checker will be released at Apr 21, 2023 12:00 PM
+ An auto review will be launched at the deadline
